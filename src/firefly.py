d7f35b7747fadef1909e9a6f74fee23fe555585c
Masaharu Kato <saa01068@gmail.com>
2018-11-09 18:38:39 +0900

    目的関数の範囲指定を行った。

--- src/firefly.py
+++ src/firefly.py
@@ -8,17 +8,16 @@ def firefly(*, x, distance, I, alpha, beta, gamma, epsilon):
     for i in range(len(x)):
         for j in range(i):
 
-            # print("|{:3}| j:[{:3}]({:+7.3f},{:+7.3f}) i:[{:3}]({:+7.3f},{:+7.3f})".format(
-            #     i_gen,
-            #     j, x[j], I(x[j]),
-            #     i, x[i], I(x[i]),
-            # ), end='')
+            print("     j:[{:3}]({:+7.3f},{:+7.3f}) i:[{:3}]({:+7.3f},{:+7.3f})".format(
+                j, x[j], I(x[j]),
+                i, x[i], I(x[i]),
+            ), end='')
             
             if I(x[i]) > I(x[j]):
                 new_x[i] += beta * math.exp(-gamma * np.power(distance(x[i], x[j]), 2)) * (x[j] - x[i]) + alpha * epsilon
-                #print(" -> {:+7.3f}".format(new_x[i]), end='')
+                print(" -> {:+7.3f}".format(new_x[i]), end='')
             
-            #print('')
+            print('')
 
-    #print('')
+    print('')
     return new_x
--- src/michalewicz.py
+++ src/michalewicz.py
@@ -3,18 +3,30 @@ import numpy as np
 # ref: https://www.sfu.ca/~ssurjano/michal.html
 
 def michalewicz(x, m):
+
     if(np.isscalar(x)):
-        ret = -np.sin(x) * np.sin(x ** 2 / np.pi) ** (2 * m)
+        if(x < 0 or x >= np.pi):
+            return np.inf
+        len_range = np.arange(1, 2)
     else:
+        for cx in x:
+            if(cx < 0 or cx >= np.pi):
+                return np.inf
         len_range = np.arange(1, len(x)+1)
-        ret = -sum(
-            np.multiply(
-                np.sin(x),
-                np.power(
-                    np.sin(np.divide(np.multiply(len_range,  np.power(x, 2)), np.pi)),
-                    2 * m
-                )
-            )
-        )
+
+#    if(np.isscalar(x)):
+    ret = -sum(np.sin(x) * np.sin((len_range * x ** 2) / np.pi) ** (2 * m))
+    # else:
+    #     len_range = np.arange(1, len(x)+1)
+    #     ret = -sum(
+    #         np.multiply(
+    #             np.sin(x),
+    #             np.power(
+    #                 np.sin(np.divide(np.multiply(len_range,  np.power(x, 2)), np.pi)),
+    #                 2 * m
+    #             )
+    #         )
+    #     )
+
     return ret
     
