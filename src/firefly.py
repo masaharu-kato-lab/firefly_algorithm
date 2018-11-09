cc324d9c50b0dc4f1d3c9905a28ba6cc938acd59
Masaharu Kato <saa01068@gmail.com>
2018-11-09 17:53:26 +0900

    1次元の連続ホタルアルゴリズムを実装

--- src/firefly.py
+++ src/firefly.py
@@ -4,10 +4,21 @@ import numpy as np
 # Basic firefly algorythm
 def firefly(*, x, distance, I, alpha, beta, gamma, epsilon):
 
-    new_x = [0] * len(x)
+    new_x = x
     for i in range(len(x)):
         for j in range(i):
-            if I(x[i]) > I(x[j]):
-                new_x[i] = x[i] + beta * math.exp(-gamma * np.power(distance(x[i], x[j]), 2)) * (x[j] - x[i]) + alpha * epsilon
 
+            # print("|{:3}| j:[{:3}]({:+7.3f},{:+7.3f}) i:[{:3}]({:+7.3f},{:+7.3f})".format(
+            #     i_gen,
+            #     j, x[j], I(x[j]),
+            #     i, x[i], I(x[i]),
+            # ), end='')
+            
+            if I(x[i]) > I(x[j]):
+                new_x[i] += beta * math.exp(-gamma * np.power(distance(x[i], x[j]), 2)) * (x[j] - x[i]) + alpha * epsilon
+                #print(" -> {:+7.3f}".format(new_x[i]), end='')
+            
+            #print('')
+
+    #print('')
     return new_x
--- src/test.py
+++ src/test.py
@@ -1,34 +1,37 @@
 from firefly import firefly
 from michalewicz import michalewicz
 import matplotlib.pyplot as plt
+import random
+import sys
 import numpy as np
+import subprocess
 
 # Set seed value of random
-# np.random.seed(123456)
+seed = random.randrange(2 ** 32 - 1)
+print('seed: {:}'.format(seed))
+np.random.seed(seed=seed)
 
-n = 10
+n = 100
 I = lambda x: michalewicz(x, 10)
+# I = lambda x: x ** 4 - 20 * x ** 2 + 20 * x
 # sd = 5.0
 # x = np.random.normal(0, sd, n)
-x = np.random.rand(n) * 2.0
+x = np.random.rand(n) * 2.0 - 1.0
 
 # Run firefly algorythm
-for i_gen in range(30):
-    x = firefly(
-        n_gen = 1,
-        x = x,
-        distance = lambda p, q: np.linalg.norm(q - p),
-        I = I,
-        alpha = lambda t: 1,
-        beta = 1,
-        gamma = 1,
-        epsilon = lambda t: 0,
-    )
+for i_gen in range(100):
+	x = firefly(
+		x = x,
+		distance = lambda p, q: np.linalg.norm(q - p),
+		I = I,
+		alpha = 1,
+		beta = 1,
+		gamma = 1,
+		epsilon = np.random.normal(0, 1.0),
+	)
+	
+	Ix = list(map(I, x))
+	min_x_i = np.argmin(Ix)
+	print('{:+9.6f} at {:+f}'.format(I(x[min_x_i]), x[min_x_i]))
 
-    xval = list(ma
-    dp(I, x))
-    xargs = np.argsort(xval)
-    print(x)
-  #  print(xval)
-
-    print(i_gen, xargs[0], I(xargs[0]))
+	#input()
