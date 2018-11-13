457c6c5fa0eff383da45e3cc13922228c31d5615
Masaharu Kato <saa01068@gmail.com>
2018-11-13 14:05:06 +0900

    Add comments in firefly function.

--- src/firefly.py
+++ src/firefly.py
@@ -1,23 +1,45 @@
 import math
 import numpy as np
+from typing import Callable
 
-# Basic firefly algorythm
-def firefly(*, x, distance, I, alpha, beta, gamma, epsilon):
-
+# Calc firefly algorithm once
+def firefly(*,
+        x        : list,         # Initial positions of fireflies
+        distance : Callable,     # Function to calculate distance between two fireflies
+        I        : Callable,     # Objective Function (Originally means light intensity of fireflies)
+        alpha    : float,        # Constant Alpha
+        beta     : float,        # Constant Beta
+        gamma    : float,        # Constant Gamma
+        epsilon  : float,        # Constant Epsilon
+        debug_out : bool = False # Whether to output information for debugging
+    ) -> list: # Returns positions of fireflies after calculation
+    
+    # New positions of fireflies
     new_x = x
+
+    # Repeats for all combinations of fireflies
     for i in range(len(x)):
         for j in range(i):
-
-            print("     j:[{:3}]({:+7.3f},{:+7.3f}) i:[{:3}]({:+7.3f},{:+7.3f})".format(
-                j, x[j], I(x[j]),
-                i, x[i], I(x[i]),
-            ), end='')
             
+            # Move firefly 'i' towards firefly 'j' if objective function value of 'j' is smaller than 'i'
             if I(x[i]) > I(x[j]):
+                # Move firefly based on formula
                 new_x[i] += beta * math.exp(-gamma * np.power(distance(x[i], x[j]), 2)) * (x[j] - x[i]) + alpha * epsilon
-                print(" -> {:+7.3f}".format(new_x[i]), end='')
-            
-            print('')
 
-    print('')
+            # Output positions and intensities of fireflies for debugging
+            if debug_out:
+                print("     j:[{:3}]({:+7.3f},{:+7.3f}) i:[{:3}]({:+7.3f},{:+7.3f})".format(
+                    j, x[j], I(x[j]),
+                    i, x[i], I(x[i]),
+                ), end='')
+
+                if I(x[i]) > I(x[j]):
+                    print(" -> {:+7.3f}".format(new_x[i]), end='') 
+                
+                print('')
+
+        if debug_out:
+            print('') 
+
+    # Returns new positions of fireflies
     return new_x
--- src/michalewicz.py
+++ src/michalewicz.py
@@ -2,31 +2,10 @@ import numpy as np
 
 # ref: https://www.sfu.ca/~ssurjano/michal.html
 
-def michalewicz(x, m):
+# calc michalewicz function
+def michalewicz(x, m : float):
 
-    if(np.isscalar(x)):
-        if(x < 0 or x >= np.pi):
-            return np.inf
-        len_range = np.arange(1, 2)
-    else:
-        for cx in x:
-            if(cx < 0 or cx >= np.pi):
-                return np.inf
-        len_range = np.arange(1, len(x)+1)
-
-#    if(np.isscalar(x)):
-    ret = -sum(np.sin(x) * np.sin((len_range * x ** 2) / np.pi) ** (2 * m))
-    # else:
-    #     len_range = np.arange(1, len(x)+1)
-    #     ret = -sum(
-    #         np.multiply(
-    #             np.sin(x),
-    #             np.power(
-    #                 np.sin(np.divide(np.multiply(len_range,  np.power(x, 2)), np.pi)),
-    #                 2 * m
-    #             )
-    #         )
-    #     )
+    dimentions = np.arange(1, (1 if np.isscalar(x) else len(x))+1)
+    ret = -sum(np.sin(x) * np.sin((dimentions * x ** 2) / np.pi) ** (2 * m))
 
     return ret
-    
