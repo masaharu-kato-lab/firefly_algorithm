c71b4b4c4e398a02f929849bb8dbacf7feb02421
Masaharu Kato <saa01068@gmail.com>
2018-12-04 16:54:05 +0900

    luminosityを関数化

--- src/discrete/opu/_run_classic.py
+++ src/discrete/opu/_run_classic.py
@@ -1,91 +1,13 @@
 import random
-import math
 import distance
 import copy
 import argparse
 import time
-import pickle
-import map_converter # Using inside mapper.pickle
-
-with open("res/mapper.pickle", "rb") as f:
-    mapper = pickle.load(f)
-
-# Firefly class
-class Firefly:
-    def __init__(self, _coords, eta, n_drones):
-        self.eta = eta
-        self.n_drones = n_drones
-
-        coords = copy.copy(_coords)
-        random.shuffle(coords)
-        self.update(coords)
-
-    # Update fireflies data
-    def update(self, coords):
-        uncertainty, distance, solution = self.evaluate(coords)
-        self.x = solution
-        self.uncertainty = uncertainty
-        self.distance = distance
-        self.luminosity = 10000 * uncertainty + self.eta * distance
-
-    # Build the paths from a list of points
-    # バッテリの関係でスタート地点に戻らなければならず, それを考慮して経路を決める
-    def evaluate(self, _coords):
-        coords = copy.copy(_coords)
-        limit = 0
-        battery = 0
-        last_position = [mapper.starting_point[i] for i in range(self.n_drones)]
-        start = copy.copy(last_position)
-        point_time = {}
-        drone_elapsed_time = [0] * self.n_drones
-        d = 0
-        solution = []
-        way = []
-
-        while len(coords) > 0:
-            
-            if limit + mapper.paths[(last_position[d], coords[0])][1] + mapper.paths[(coords[0], start[d])][1] < 3000:
-                path = mapper.paths[(last_position[d], coords[0])][1]
-                limit += path
-                drone_elapsed_time[d] += path * 0.5
-                point_time[coords[0]] = drone_elapsed_time[d]
-                way.append(coords[0])
-                last_position[d] = coords.pop(0)
-
-            else:
-                solution.append(way)
-                way = []
-                path = mapper.paths[(last_position[d], start[d])][1]
-                battery += limit + path
-                limit = 0
-                drone_elapsed_time[d] += path * 0.5
-                last_position[d] = start[d]
-                d += 1
-                if d >= self.n_drones:
-                    d = 0
-
-        for d in range(self.n_drones):
-            if last_position[d] != start[d]:
-                solution.append(way)
-                path = mapper.paths[(last_position[d], start[d])][1]
-                battery += limit + path
-                drone_elapsed_time[d] += path * 0.5
-
-        end_patrol_time = max(drone_elapsed_time)
-        mean = []
-
-        for element in list(point_time.values()):
-            mean.append(1 - math.exp(-0.001279214 * (end_patrol_time - element)))
-
-        uncertainty = sum(mean) / len(mean)
-
-        return uncertainty, battery, solution
-
+import firefly
 
 
 def flatten(a):
     return [element for sublist in a for element in sublist]
-
 
 
 # Beta step: exploitation
@@ -159,10 +81,9 @@ def fireflyAlgorithm(z, args):
     if args.verbose: print(args)
 
     points = [(32, 1122), (271, 1067), (209, 993), (184, 1205), (303, 1220), (400, 1122), (505, 1214), (669, 1202), (779, 1026), (912, 1029), (1483, 1156), (1614, 991), (1576, 567), (1502, 395), (1618, 227), (1448, 634), (967, 690), (1059, 842), (759, 823), (1387, 174), (1073, 82), (944, 327), (866, 512), (748, 638), (487, 896), (118, 653), (35, 902), (502, 339), (683, 316), (694, 123), (45, 52), (367, 39)]
-    swarm = [Firefly(points, args.dist_penal_coef, args.n_drones) for i in range(args.n_fireflies)]
+    swarm = [firefly.Firefly(points, args.dist_penal_coef, args.n_drones) for i in range(args.n_fireflies)]
     swarm = sorted(swarm, key = lambda ff: ff.luminosity)
     best_firefly = copy.deepcopy(swarm[0])
-
 
     if args.verbose: print("Best firefly init: ", best_firefly.luminosity)
 
@@ -174,7 +95,7 @@ def fireflyAlgorithm(z, args):
         for i in range(n):
             for j in range(n):
                 if j != i:
-                    if swarm[j].luminosity < swarm[i].luminosity:
+                    if firefly.luminosity(swarm[j]) < firefly.luminosity(swarm[i]):
                         c = betaStep(swarm[j].x, swarm[i].x, args.gamma)
                         c = alphaStep(flatten(swarm[i].x), args.alpha)
                         swarm[i].update(c)
@@ -210,16 +131,16 @@ def fireflyAlgorithm(z, args):
 
 if __name__ == "__main__":
     # Arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--n_drones'        , type = int  , default = 2     , help = 'number of drones')
-    parser.add_argument('-i', '--n_iteration'     , type = int  , default = 3000  , help = 'number of iterations')
-    parser.add_argument('-g', '--gamma'           , type = float, default = 0.90  , help = 'firefly algorithm gamma')
-    parser.add_argument('-a', '--alpha'           , type = int  , default = 1     , help = 'firefly algorithm alpha')
-    parser.add_argument('-f', '--n_fireflies'     , type = int  , default = 10    , help = 'number of fireflies')
-    parser.add_argument('-e', '--dist_penal_coef' , type = float, default = 0.1   , help = 'distance penalization coeficient')
-    parser.add_argument('-p', '--verbose'         , type = int  , default = False , help = 'enable/desable verbose')
-    parser.add_argument('-s', '--seed'            , type = int  , default = 853295828 , help = 'seed value (Randomly determined if not specified)')
-    args = parser.parse_args()
+    argp = argparse.ArgumentParser()
+    argp.add_argument('-d', '--n_drones'        , type = int  , default = 2         , help = 'number of drones')
+    argp.add_argument('-i', '--n_iteration'     , type = int  , default = 3000      , help = 'number of iterations')
+    argp.add_argument('-g', '--gamma'           , type = float, default = 0.90      , help = 'firefly algorithm gamma')
+    argp.add_argument('-a', '--alpha'           , type = int  , default = 1         , help = 'firefly algorithm alpha')
+    argp.add_argument('-f', '--n_fireflies'     , type = int  , default = 10        , help = 'number of fireflies')
+    argp.add_argument('-e', '--dist_penal_coef' , type = float, default = 0.1       , help = 'distance penalization coeficient')
+    argp.add_argument('-s', '--seed'            , type = int  , default = 853295828 , help = 'seed value (Randomly determined if not specified)')
+    argp.add_argument(      '--verbose' ,                 action="store_true"       , help='Whether to output details for debugging')
+    args = argp.parse_args()
 
     if args.seed == None:
         args.seed = random.randrange(2 ** 32 - 1)
---
+++ src/discrete/opu/firefly.py
@@ -0,0 +1,65 @@
+import copy
+import math
+import random
+import pickle
+import map_converter # Using inside mapper.pickle
+
+with open("res/mapper.pickle", "rb") as f:
+    mapper = pickle.load(f)
+
+
+# Build the paths from a list of points
+# バッテリの関係でスタート地点に戻らなければならず, それを考慮して経路を決める
+def luminosity(_coords, *, n_drones, eta):
+    coords = copy.copy(_coords)
+    limit = 0
+    distance = 0
+    last_position = [mapper.starting_point[i] for i in range(n_drones)]
+    start = copy.copy(last_position)
+    point_time = {}
+    drone_elapsed_time = [0] * n_drones
+    d = 0
+    solution = []
+    way = []
+
+    while len(coords) > 0:
+        
+        if limit + mapper.paths[(last_position[d], coords[0])][1] + mapper.paths[(coords[0], start[d])][1] < 3000:
+            path = mapper.paths[(last_position[d], coords[0])][1]
+            limit += path
+            drone_elapsed_time[d] += path * 0.5
+            point_time[coords[0]] = drone_elapsed_time[d]
+            way.append(coords[0])
+            last_position[d] = coords.pop(0)
+
+        else:
+            solution.append(way)
+            way = []
+            path = mapper.paths[(last_position[d], start[d])][1]
+            distance += limit + path
+            limit = 0
+            drone_elapsed_time[d] += path * 0.5
+            last_position[d] = start[d]
+            d += 1
+            if d >= n_drones:
+                d = 0
+
+    for d in range(n_drones):
+        if last_position[d] != start[d]:
+            solution.append(way)
+            path = mapper.paths[(last_position[d], start[d])][1]
+            distance += limit + path
+            drone_elapsed_time[d] += path * 0.5
+
+    end_patrol_time = max(drone_elapsed_time)
+    mean = []
+
+    for element in list(point_time.values()):
+        mean.append(1 - math.exp(-0.001279214 * (end_patrol_time - element)))
+
+    uncertainty = sum(mean) / len(mean)
+
+    luminosity = 10000 * uncertainty + eta * distance
+
+    return luminosity
+
