47c8d861ce94e670e14bfbfa4e15aad55f3901d6
Masaharu Kato <saa01068@gmail.com>
2018-12-13 04:54:51 +0900

    fixed shell

--- batch/search_init.sh
+++ batch/search_init.sh
@@ -1,6 +1,6 @@
 source env/bin/activate
-for i in `seq 1 100`
+for i in `seq 1 1000`
 do
-    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -s $i -is $i -fnr -i random  --stdout
-    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -s $i -is $i -fnr -i nn      --stdout
+    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -fnr -i random
+    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -fnr -i nn
 done
