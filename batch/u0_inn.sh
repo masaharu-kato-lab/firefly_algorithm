f014056383098fa45aab954830653735f5b1a7b1
Masaharu Kato <saa01068@gmail.com>
2018-12-18 15:57:06 +0900

    modified batch

--- batch/u0_inn.sh
+++ batch/u0_inn.sh
@@ -2,5 +2,5 @@
 source env/bin/activate
 for i in `seq 1 100`
 do
-    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -fnr -u 0 -i nn --stdout > logs/u0_inn/$i.txt
+    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -fnr -u 0 -i nn --stdout > logs/u0_inn/u0_inn_$(print %04d $i).txt
 done
