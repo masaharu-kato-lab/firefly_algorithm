48150ed7d1285f3abe668b99850e4972dc2d973e
Masaharu Kato (lab-CKG) <saa01068@gmail.com>
2019-10-09 21:44:03 +0900

    delete batches

--- batch/search_init.sh
+++
@@ -1,6 +0,0 @@
-source env/bin/activate
-for i in `seq 1 1000`
-do
-    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -fnr -i random
-    python src/discrete/_run_opu.py -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 1000 -d 2 -fnr -i nn
-done
--- batch/u0_t10k_ird.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-log_path=logs/$(basename "$0" .sh)
-
-mkdir -p $log_path
-
-for i in `seq 1 100`
-do
-    env/bin/python src/discrete/_run_opu.py -s $i -is $i -n 93 -g 0.01502 -a 6.0 -ba 6.0 -e 0.1 -t 10000 -d 2 -fnr -u 0 -i random --stdout > $log_path/$(printf %04d $i).txt
-done
