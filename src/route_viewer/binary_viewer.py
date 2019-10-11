7a9b3dd21c606851d3f2396320a7c1474a43cbd9
Masaharu Kato (lab-CKG) <saa01068@gmail.com>
2019-10-11 13:57:27 +0900

    add binary viewer

--- src/route_viewer/binary_viewer.py
+++ src/route_viewer/binary_viewer.py
@@ -1,5 +1,9 @@
 import pickle
 import argparse
+import matplotlib.pyplot as plt
+import sys
+import os
+sys.path.append(os.path.dirname(__file__) + '/../route_planner')
 
 def main():
 
@@ -10,8 +14,22 @@ def main():
     with open(args.input, mode='rb') as f:
         out_bin = pickle.load(f)
 
-    print(out_bin)
+    for seed, last_ret in out_bin.lasts.items():
+        print('seed:{}'.format(seed))
+        plan = last_ret.best_plan
 
+        for i, drone in enumerate(plan.drones):
+            # print('drone {}:'.format(i), end='')
+            # print(drone.pos_history)
+            ps = drone.pos_history
+            plt.plot(
+                [p[0] for p in ps],
+                [p[1] for p in ps],
+                label='drone {}:'.format(i)
+            )
+        
+        plt.show()
+    
 
 if __name__ == '__main__':
     main()
