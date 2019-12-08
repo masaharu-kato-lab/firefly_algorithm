f33e9d8af1a0d04da3db099c3191a7356f33d688
Masaharu Kato (Desktop) <fudai.mk@gmail.com>
2019-12-09 05:34:54 +0900

    a

--- src/route_viewer/value_stat.py
+++ src/route_viewer/value_stat.py
@@ -17,17 +17,23 @@ def main():
 
     argp = argparse.ArgumentParser(description='Route binary viewer')
     argp.add_argument('inputs', type=str, nargs='+', help='Input binary pickle file path')
-    argp.add_argument('-x', '--x', type=int, nargs='+', required=True, help='X value (number of updates)')
+    argp.add_argument('-o'   , '--output'    , type=str, required=True, help='Output path statistics file')
+    argp.add_argument('-xmax', '--x_max'     , type=int, required=True, help='Maximum X value')
+    argp.add_argument('-xinv', '--x_interval', type=int, default=1, help='X value interval')
     args = argp.parse_args()
 
+    xlist = range(0, args.x_max+1, args.x_interval)
+    
     for i, _input in enumerate(args.inputs):
         with open(_input, mode='rb') as f:
             binary = pickle.load(f)
             states_list = binary.states_by_seed.values()
 
-        values = [[*states_to_values_by_update(states)] for states in states_list]
-        yvals = 
-
+        with open(args.output, mode='w') as f:
+            print('\t'.join('{:11}'.format(x) for x in xlist), file=f)
+            for states in states_list:
+                values = [*states_to_values_by_update(states)]
+                print('\t'.join('{:11.3f}'.format(v) for v in (get_value_from_values(values, x) for x in xlist)), file=f)
 
 
 
@@ -36,6 +42,10 @@ def states_to_values_by_update(states) -> Iterator[float]:
         value = state.best_plan.value
         for _ in range(state.current_n_updates):
             yield value
+
+
+def get_values(values_list:List[List[float]], xlist:Iterable[int]) -> List[List[float]]:
+    return [[get_value_from_values(values, x) for values in values_list] for x in xlist]
 
 
 def average_values(values_list:List[List[float]], xlist:Iterable[int]) -> List[float]:
