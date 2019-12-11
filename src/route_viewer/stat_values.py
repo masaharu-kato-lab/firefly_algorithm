f91a97422dc8f49ee106068feee842deaca39e3b
Masaharu Kato <saa01068@gmail.com>
2019-12-12 00:40:56 +0900

    modified stat_values

--- src/route_viewer/stat_values.py
+++ src/route_viewer/stat_values.py
@@ -4,6 +4,8 @@ import argparse
 import pickle
 import sys
 import os
+from itertools import chain
+import statistics
 
 from attrdict import AttrDict #type:ignore
 
@@ -16,11 +18,12 @@ def main():
     argp = argparse.ArgumentParser(description='Route binary viewer')
     argp.add_argument('input', nargs='+', type=str, help='Input binary pickle file path')
     argp.add_argument('-xmax', '--x_max'     , type=int, required=True, help='Maximum X value')
-    argp.add_argument('-xinv', '--x_interval', type=int, default=1, help='X value interval')
+    argp.add_argument('-xinv', '--x_interval', type=int, required=True, help='X value interval')
     args = argp.parse_args()
 
     xlist = range(0, args.x_max+1, args.x_interval)
-    print('\t'.join(['{:20}'.format(''), *('{:11}'.format(x) for x in xlist)]))
+    print('\t'.join(['{:20}'.format(''), *('{:>10}'.format(x) for x in chain.from_iterable([x, '', '', ''] for x in xlist))]))
+    print('\t'.join(['{:20}'.format(''), *('{:>10}'.format(x) for x in ['min', 'max', 'mean', 'sd'] * len(xlist))]))
     
     for _input in args.input:
         with open(_input, mode='rb') as f:
@@ -30,7 +33,18 @@ def main():
         name = os.path.splitext(os.path.basename(_input))[0]
         values = [[*states_to_values_by_update(states)] for states in states_list]
 
-        print('\t'.join(['{:20}'.format(name), *('{:11.3f}'.format(v) for v in average_values(values, xlist))]))
+        print('\t'.join([
+            '{:20}'.format(name),
+            *chain.from_iterable(
+                ['{:10.03f}'.format(vmin), '{:10.03f}'.format(vmax), '{:10.03f}'.format(ave), '{:10.03f}'.format(sd)]
+                for vmin, vmax, ave, sd in zip(
+                    min_values(values, xlist),
+                    max_values(values, xlist),
+                    average_values(values, xlist),
+                    sd_values(values, xlist)
+                )
+            )
+        ]))
 
 
 
@@ -50,6 +64,17 @@ def average_values(values_list:List[List[float]], xlist:Iterable[int]) -> List[f
     n = len(values_list)
     return [sum(get_value_from_values(values, x) for values in values_list) / n for x in xlist]
 
+
+def sd_values(values_list:List[List[float]], xlist:Iterable[int]) -> List[float]:
+    return [statistics.stdev(get_value_from_values(values, x) for values in values_list) for x in xlist]
+
+
+def min_values(values_list:List[List[float]], xlist:Iterable[int]) -> List[float]:
+    return [min(get_value_from_values(values, x) for values in values_list) for x in xlist]
+
+
+def max_values(values_list:List[List[float]], xlist:Iterable[int]) -> List[float]:
+    return [max(get_value_from_values(values, x) for values in values_list) for x in xlist]
 
 if __name__ == '__main__':
     main()
