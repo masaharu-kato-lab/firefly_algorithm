e9e5007bf0427ed02b792c14e37423d9e14372b7
Masaharu Kato <saa01068@gmail.com>
2020-04-16 12:02:31 +0900

    add map's text dump function

--- src/route_viewer/mapper_image.py
+++ src/route_viewer/mapper_image.py
@@ -2,6 +2,7 @@
 import argparse
 import matplotlib.pyplot as plt #type:ignore
 import os
+import sys
 from world import World
 
 def main():
@@ -9,9 +10,21 @@ def main():
     argp = argparse.ArgumentParser(description='Route binary viewer')
     argp.add_argument('-o', '--output', type=str, default=None, help='Output png image file path (None:default)')
     argp.add_argument('-mi', '--mapper_input', type=str, default='res/pathdata/opu.pickle', help='Input mapper pickle file path')
+    argp.add_argument('-td', '--text_dump', action='store_true', help='Text dump mode')
     args = argp.parse_args()
 
     world = World(args.mapper_input)
+
+    if args.text_dump:
+        if args.output:
+            os.makedirs(os.path.dirname(args.output), exist_ok=True)
+            with open(args.output, mode='w') as f:
+                world.dump_world(f)
+        else:
+            world.dump_world(sys.stdout)
+            
+        return
+
 
     plt.figure()
     world.plot_world(color='#BBBBBB')
